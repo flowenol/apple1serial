7de77dd557de8506fc9c78d88ad757e8e07f287c
Piotr Jaczewski <piotr.jaczewski@rtbhouse.com>
2020-05-25 00:53:53 +0200

    Timings for write fixed, software reset added

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xa -W -C -v -O ASCII -c src/apple1serial.xa -l apple1serial.label -o apple1serial.bin
+xa -W -C -v -O ASCII -S -c src/apple1serial.xa -l apple1serial.label -o apple1serial.bin
