a7c69a9964ed21d4436c8559e1b5a51ac691d1fb
Piotr Jaczewski <piotr.jaczewski@rtbhouse.com>
2020-09-27 21:22:34 +0200

    serial monitor added

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 xa -W -C -v -O ASCII -S -c src/apple1serial.xa -l apple1serial.label -o apple1serial.bin
+#xa -W -C -v -O ASCII -S -c src/serialmonitor.xa -l serialmonitor.label -o serialmonitor.bin
